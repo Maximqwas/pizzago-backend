17c587e5aa82d51f158a53f7a112cc263298d8e0
jayeer <illicibi01@gmail.com>
2025-05-28 16:21:31 +0300

    Moved application to separate app.js file to make testing easier. Driver code (including app.listen) now resides in index.js, while routes and all setup now lives in index.js

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-
-// Load environment variables from .env file
+const dotenv = require("dotenv");
 dotenv.config();
 
-const app = express();
+const app = require("./app.js");
+
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// TODO: Add routes here
-
-// Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
